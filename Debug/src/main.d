b108a30ff36d2951796bdb2648be12eab8de6ee1
rkroshan <rkroshan.1999@gmail.com>
2020-12-27 19:20:02 +0530

    sucessfully tested 2 bare minimum task creation and context switching in FreeRTOS

--- Debug/src/main.d
+++ Debug/src/main.d
@@ -30,7 +30,16 @@ src/main.o: ../src/main.c \
  /home/admirer/Desktop/master_mcu_stm32/FreeRTOS_STM32_Porting/FreeRTOS_STM32_Porting/StdPeriph_Driver/inc/stm32f4xx_rng.h \
  /home/admirer/Desktop/master_mcu_stm32/FreeRTOS_STM32_Porting/FreeRTOS_STM32_Porting/StdPeriph_Driver/inc/stm32f4xx_can.h \
  /home/admirer/Desktop/master_mcu_stm32/FreeRTOS_STM32_Porting/FreeRTOS_STM32_Porting/StdPeriph_Driver/inc/stm32f4xx_dac.h \
- /home/admirer/Desktop/master_mcu_stm32/FreeRTOS_STM32_Porting/FreeRTOS_STM32_Porting/StdPeriph_Driver/inc/stm32f4xx_dcmi.h
+ /home/admirer/Desktop/master_mcu_stm32/FreeRTOS_STM32_Porting/FreeRTOS_STM32_Porting/StdPeriph_Driver/inc/stm32f4xx_dcmi.h \
+ /home/admirer/Desktop/master_mcu_stm32/FreeRTOS_STM32_Porting/FreeRTOS_STM32_Porting/FreeRTOS/Source/include/FreeRTOS.h \
+ /home/admirer/Desktop/master_mcu_stm32/FreeRTOS_STM32_Porting/FreeRTOS_STM32_Porting/inc/FreeRTOSConfig.h \
+ /home/admirer/Desktop/master_mcu_stm32/FreeRTOS_STM32_Porting/FreeRTOS_STM32_Porting/FreeRTOS/Source/include/projdefs.h \
+ /home/admirer/Desktop/master_mcu_stm32/FreeRTOS_STM32_Porting/FreeRTOS_STM32_Porting/FreeRTOS/Source/include/portable.h \
+ /home/admirer/Desktop/master_mcu_stm32/FreeRTOS_STM32_Porting/FreeRTOS_STM32_Porting/FreeRTOS/Source/include/deprecated_definitions.h \
+ /home/admirer/Desktop/master_mcu_stm32/FreeRTOS_STM32_Porting/FreeRTOS_STM32_Porting/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h \
+ /home/admirer/Desktop/master_mcu_stm32/FreeRTOS_STM32_Porting/FreeRTOS_STM32_Porting/FreeRTOS/Source/include/mpu_wrappers.h \
+ /home/admirer/Desktop/master_mcu_stm32/FreeRTOS_STM32_Porting/FreeRTOS_STM32_Porting/FreeRTOS/Source/include/task.h \
+ /home/admirer/Desktop/master_mcu_stm32/FreeRTOS_STM32_Porting/FreeRTOS_STM32_Porting/FreeRTOS/Source/include/list.h
 
 /home/admirer/Desktop/master_mcu_stm32/FreeRTOS_STM32_Porting/FreeRTOS_STM32_Porting/CMSIS/device/stm32f4xx.h:
 
@@ -95,3 +104,21 @@ src/main.o: ../src/main.c \
 /home/admirer/Desktop/master_mcu_stm32/FreeRTOS_STM32_Porting/FreeRTOS_STM32_Porting/StdPeriph_Driver/inc/stm32f4xx_dac.h:
 
 /home/admirer/Desktop/master_mcu_stm32/FreeRTOS_STM32_Porting/FreeRTOS_STM32_Porting/StdPeriph_Driver/inc/stm32f4xx_dcmi.h:
+
+/home/admirer/Desktop/master_mcu_stm32/FreeRTOS_STM32_Porting/FreeRTOS_STM32_Porting/FreeRTOS/Source/include/FreeRTOS.h:
+
+/home/admirer/Desktop/master_mcu_stm32/FreeRTOS_STM32_Porting/FreeRTOS_STM32_Porting/inc/FreeRTOSConfig.h:
+
+/home/admirer/Desktop/master_mcu_stm32/FreeRTOS_STM32_Porting/FreeRTOS_STM32_Porting/FreeRTOS/Source/include/projdefs.h:
+
+/home/admirer/Desktop/master_mcu_stm32/FreeRTOS_STM32_Porting/FreeRTOS_STM32_Porting/FreeRTOS/Source/include/portable.h:
+
+/home/admirer/Desktop/master_mcu_stm32/FreeRTOS_STM32_Porting/FreeRTOS_STM32_Porting/FreeRTOS/Source/include/deprecated_definitions.h:
+
+/home/admirer/Desktop/master_mcu_stm32/FreeRTOS_STM32_Porting/FreeRTOS_STM32_Porting/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h:
+
+/home/admirer/Desktop/master_mcu_stm32/FreeRTOS_STM32_Porting/FreeRTOS_STM32_Porting/FreeRTOS/Source/include/mpu_wrappers.h:
+
+/home/admirer/Desktop/master_mcu_stm32/FreeRTOS_STM32_Porting/FreeRTOS_STM32_Porting/FreeRTOS/Source/include/task.h:
+
+/home/admirer/Desktop/master_mcu_stm32/FreeRTOS_STM32_Porting/FreeRTOS_STM32_Porting/FreeRTOS/Source/include/list.h:
--- src/main.c
+++ src/main.c
@@ -10,11 +10,51 @@
 
 
 #include "stm32f4xx.h"
-			
+#include "FreeRTOS.h"
+#include "task.h"
+
+#define TASK1_PRIORITY 2			//remember task priority can never be 0 as idle task priority is 0
+#define TASK2_PRIORITY 2
+
+TaskHandle_t xTask1_Handle = NULL;
+TaskHandle_t xTask2_Handle = NULL;
+
+void vTask1_Handler(void* params);
+void vTask2_Handler(void* params);
 
 int main(void)
 {
+	//By default the system clk is running at max speed which 168Mhz
+	//this is done by setsysclk function
+	//if it is not required then deinit the clk config and set it to desired value using functions provided in stm32f4xx_rcc.c
+	RCC_DeInit();
+	//set HSE as default clk
+	RCC_SYSCLKConfig(RCC_SYSCLKSource_HSE);
+	//call the systemcoreclk update function the SystemCoreClock variable to newly set clk frequency
+	SystemCoreClockUpdate();
+
+	//create 1st task
+	xTaskCreate(vTask1_Handler, "Led_1", configMINIMAL_STACK_SIZE, NULL, TASK1_PRIORITY, &xTask1_Handle);
+	xTaskCreate(vTask2_Handler, "Led_2", configMINIMAL_STACK_SIZE, NULL, TASK2_PRIORITY, &xTask2_Handle);
+
+	//start the FreeRtos Task Scheduler
+	vTaskStartScheduler();
 
 
 	for(;;);
+}
+
+void vTask1_Handler(void* params)
+{
+	//task must never return
+	while(1){};
+	//if it returns anyhow or task work is complete it is not repeating then delete the task
+	vTaskDelete(NULL); //NULL means the task itself or can call the vtask1_handler
+}
+void vTask2_Handler(void* params)
+{
+	//task must never return
+	while(1){};
+	//if it returns anyhow or task work is complete it is not repeating then delete the task
+	vTaskDelete(xTask2_Handle); //NULL means the task itself
 }
