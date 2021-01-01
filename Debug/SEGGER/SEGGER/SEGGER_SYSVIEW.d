00c551b3466d06f0a84cd485c0d6dba53a39f968
rkroshan <rkroshan.1999@gmail.com>
2021-01-02 00:46:24 +0530

    Successfully integrate segger with freeRtosv20 and tested in single shot recording

---
+++ Debug/SEGGER/SEGGER/SEGGER_SYSVIEW.d
@@ -0,0 +1,24 @@
+SEGGER/SEGGER/SEGGER_SYSVIEW.o: ../SEGGER/SEGGER/SEGGER_SYSVIEW.c \
+ ../SEGGER/SEGGER/SEGGER_SYSVIEW_Int.h ../SEGGER/SEGGER/SEGGER_SYSVIEW.h \
+ ../SEGGER/SEGGER/SEGGER.h \
+ /home/admirer/Desktop/master_mcu_stm32/FreeRTOS_STM32_Porting/FreeRTOS_STM32_Porting/SEGGER/Config/Global.h \
+ ../SEGGER/SEGGER/SEGGER_SYSVIEW_ConfDefaults.h \
+ /home/admirer/Desktop/master_mcu_stm32/FreeRTOS_STM32_Porting/FreeRTOS_STM32_Porting/SEGGER/Config/SEGGER_SYSVIEW_Conf.h \
+ /home/admirer/Desktop/master_mcu_stm32/FreeRTOS_STM32_Porting/FreeRTOS_STM32_Porting/SEGGER/Config/SEGGER_RTT_Conf.h \
+ ../SEGGER/SEGGER/SEGGER_RTT.h
+
+../SEGGER/SEGGER/SEGGER_SYSVIEW_Int.h:
+
+../SEGGER/SEGGER/SEGGER_SYSVIEW.h:
+
+../SEGGER/SEGGER/SEGGER.h:
+
+/home/admirer/Desktop/master_mcu_stm32/FreeRTOS_STM32_Porting/FreeRTOS_STM32_Porting/SEGGER/Config/Global.h:
+
+../SEGGER/SEGGER/SEGGER_SYSVIEW_ConfDefaults.h:
+
+/home/admirer/Desktop/master_mcu_stm32/FreeRTOS_STM32_Porting/FreeRTOS_STM32_Porting/SEGGER/Config/SEGGER_SYSVIEW_Conf.h:
+
+/home/admirer/Desktop/master_mcu_stm32/FreeRTOS_STM32_Porting/FreeRTOS_STM32_Porting/SEGGER/Config/SEGGER_RTT_Conf.h:
+
+../SEGGER/SEGGER/SEGGER_RTT.h:
--- inc/FreeRTOSConfig.h
+++ inc/FreeRTOSConfig.h
@@ -88,6 +88,8 @@ to exclude the API function. */
 #define INCLUDE_vTaskDelayUntil			1
 #define INCLUDE_vTaskDelay				1
 
+#define INCLUDE_xTaskGetIdleTaskHandle	1
+#define INCLUDE_pxTaskGetStackStart		1
 /* Cortex-M specific definitions. */
 #ifdef __NVIC_PRIO_BITS
 	/* __BVIC_PRIO_BITS will be specified when CMSIS is being used. */
@@ -123,5 +125,6 @@ standard names. */
 #define xPortPendSVHandler PendSV_Handler
 #define xPortSysTickHandler SysTick_Handler
 
+#include "SEGGER_SYSVIEW_FreeRTOS.h"
 #endif /* FREERTOS_CONFIG_H */
 
